8865fe939bb12f735ad042cb458de86c97c53c49
mRodriguez1821 <mrodriguez@fmp.com.do>
2025-10-13 10:06:16 -0400

    ajuste accion controller renombre archivo

--- API_PGI/Controllers/Accion/AccionController.cs
+++ API_PGI/Controllers/Accion/AccionController.cs
@@ -8,126 +8,120 @@ using PGI.DataAccess.Repositories.Auth;
 
 namespace API_PGI.Controllers.Accions
 {
-    //[ProducesResponseType(StatusCodes.Status200OK)]
-    //[ProducesResponseType(StatusCodes.Status201Created)]
-    //[ProducesResponseType(StatusCodes.Status400BadRequest)]
-    //[ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    //[ProducesResponseType(StatusCodes.Status403Forbidden)]
-    //[ProducesResponseType(StatusCodes.Status404NotFound)]
-    //[Route("[controller]")]
-    //[ApiController]
-    //[JwtAuthorize]
-    //public class AccionController : ControllerBase
-    //{
-    //    private readonly IAccion _Accion;
-    //    private IAuth _Auth { get; }
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [Route("[controller]")]
+    [ApiController]
+    [JwtAuthorize]
+    public class AccionController(IAccion accion, IAuth auth) : ControllerBase
+    {
 
-    //    public AccionController(IAccion accion, IAuth auth)
-    //    {
-    //        _Accion = accion;
-    //        _Auth = auth;
-    //    }
-
-    //    [HttpPost]
-    //    public IActionResult post([FromBody] Accion entity)
-    //    {
-    //        try
-    //        {
-    //            _Accion.AddSaving( entity);
+        [HttpPost]
+        public IActionResult post([FromBody] Accion entity)
+        {
+            try
+            {
+                accion.AddSaving(entity);
 
 
-    //            return Ok(new ResponseModel()
-    //            {
-    //                Result = "Guardado"
-    //            });
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            return BadRequest(new ResponseModel()
-    //            {
-    //                Error = true,
-    //                Message = ex.Message
-    //            });
-    //        }
+                return Ok(new ResponseModel()
+                {
+                    Result = "Guardado"
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseModel()
+                {
+                    Error = true,
+                    Message = ex.Message
+                });
+            }
 
-    //    }
-    //    [HttpPut]
-    //    public IActionResult Put([FromBody] Accion entity)
-    //    {
-    //        try
-    //        {
+        }
+        [HttpPut]
+        public IActionResult Put([FromBody] Accion entity)
+        {
+            try
+            {
 
-    //            _Accion.UpdateSaving( entity);
+                accion.UpdateSaving(entity);
 
-    //            return Ok(new ResponseModel()
-    //            {
-    //                Result = "Guardado"
-    //            });
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            return BadRequest(new ResponseModel()
-    //            {
-    //                Error = true,
-    //                Message = ex.Message
-    //            });
-    //        }
+                return Ok(new ResponseModel()
+                {
+                    Result = "Guardado"
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseModel()
+                {
+                    Error = true,
+                    Message = ex.Message
+                });
+            }
 
-    //    }
-    //    [HttpGet("GetAll")]
-    //    public IActionResult GetAll([FromQuery] GridifyQuery gridifyQuery)
-    //    {
-    //        try
-    //        {
-    //            var builder = new QueryBuilder<Accion>()
-    //                         .AddQuery(gridifyQuery)
-    //                         .AddCondition($"{nameof(Accion.CompaniaId)}={_Auth.CurrentUser?.CompaniaId}")
-    //            ;
-    //            if (gridifyQuery.PageSize == 0) gridifyQuery.PageSize = int.MaxValue;
-    //            if (gridifyQuery.Page == 0) gridifyQuery.Page = 1;
+        }
+        [HttpGet("GetAll")]
+        public IActionResult GetAll([FromQuery] GridifyQuery gridifyQuery)
+        {
+            try
+            {
+                var builder = new QueryBuilder<Accion>()
+                             .AddQuery(gridifyQuery)
+                             .AddCondition($"{nameof(Accion.CompaniaId)}={auth.CurrentCompany?.Id}")
+                ;
+                if (gridifyQuery.PageSize == 0) gridifyQuery.PageSize = int.MaxValue;
+                if (gridifyQuery.Page == 0) gridifyQuery.Page = 1;
 
-    //            var items = _Accion.GetPaginated(gridifyQuery);
-    //            return Ok(new ResponseModel()
-    //            {
-    //                TotalCount = items.Count,
-    //                Result = items.Data,
-    //            });
-    //            //  }
-    //        }
-    //        catch (Exception ex)
-    //        {
+                var items = accion.GetPaginated(gridifyQuery);
+                return Ok(new ResponseModel()
+                {
 
-    //            return BadRequest(new ResponseModel()
-    //            {
+                    TotalCount = items.Count,
+                    Result = items.Data,
+                });
+                //  }
+            }
+            catch (Exception ex)
+            {
 
-    //                Error = true,
-    //                Result = ex.Message
-    //            });
-    //        }
+                return BadRequest(new ResponseModel()
+                {
 
-    //    }
-    //    [HttpGet("Get/{id}")]
-    //    public IActionResult GetAll(string id)
-    //    {
-    //        try
-    //        {
-    //            var valor = _Accion.Find(x => x.Id == id);
+                    Error = true,
+                    Result = ex.Message
+                });
+            }
 
-    //            return Ok(new ResponseModel()
-    //            {
-    //                Result = valor
-    //            });
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            return BadRequest(new ResponseModel()
-    //            {
-    //                Error = true,
-    //                Message = ex.Message
-    //            });
-    //        }
+        }
+        
+        [HttpGet("Get/{id}")]
+        public IActionResult GetAll(string id)
+        {
+            try
+            {
+                var valor = accion.Find(x => x.Id == id);
+
+                return Ok(new ResponseModel()
+                {
+                    Result = valor
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseModel()
+                {
+                    Error = true,
+                    Message = ex.Message
+                });
+            }
 
 
-    //    }
-    //}
+        }
+    }
 }
